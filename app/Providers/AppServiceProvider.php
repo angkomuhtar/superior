a26613c2a0793fa9ec2b6421898e55057f05b921
angkomuhtar <angkomuhtar@gmail.com>
2024-10-23 12:09:41 +0800

    sas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,8 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV') === 'production') {
-            URL::forceSchema('https');
+        if (config('app.env') == 'production') {
+            # code...
+            \URL::forceScheme('https');
         }
     }
 }
